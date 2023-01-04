256a3983f92e4b5f8019d9ccbaadbdd7842d484a
LiWeiqiang <1072864729@qq.com>
2023-01-04 22:45:56 +0800

    build

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports =  {
     http: {
+        // baseUrl: "http://120.79.163.32:8080",
         baseUrl: "http://localhost:8080",
         // token字符串的name
         header: "Authorization"
