ec4efef4e30dd5444f213ee1b2bf82d54dbcc0d8
lwq7417 <1072864729@qq.com>
2023-02-20 16:32:45 +0800

    router

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/lib/locale/zh_CN';
 import { Provider } from 'react-redux';
-import { baseRoutes } from "./package/router/index";
+import { baseRoutes, initRouter, routeTitle } from "./package/router/index";
 import store from './package/store';
-import { initRouter } from './package/router/index';
+import { useEffect } from 'react';
 
 
 
 export default function App() {
   const router = createBrowserRouter(baseRoutes)
   initRouter(router)
+
+  useEffect(() => {
+    document.title = (routeTitle)[location.pathname] || (routeTitle)["*"]
+  }, [])
+
   return (
     <Provider store={store}>
       {/* 设置中文主题 */}
