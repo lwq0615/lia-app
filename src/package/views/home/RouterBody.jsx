f6c3692c4e41c973893d750cc7569fc39bbbd966
lwq7417 <1072864729@qq.com>
2023-02-24 15:03:12 +0800

    首页

--- src/package/views/home/RouterBody.jsx
+++ src/package/views/home/RouterBody.jsx
@@ -10,41 +10,42 @@ import Welcome from '../Welcome';
 
 function RouterBody(props) {
 
-    const [routers, setRouters] = useState([])
+  const [routers, setRouters] = useState([])
 
-    useEffect(() => {
-        const routers = createRoutes(props.routers)
-        Promise.all(routers).then(list => {
-            setRouters(list)
-        })
-    }, [props.routers])
+  useEffect(() => {
+    const routers = createRoutes(props.routers)
+    Promise.all(routers).then(list => {
+      setRouters(list)
+    })
+  }, [props.routers])
 
-    return (
-        // 路由缓存
-        <AliveScope>
-            {/* 路由切换过渡动画 */}
-            <SwitchTransition mode="out-in">
-                <CSSTransition
-                    key={props.location.key}
-                    timeout={200}
-                    classNames="route"
-                >
-                    <Routes location={props.location}>
-                        <Route
-                            key='welcome'
-                            path="*"
-                            element={
-                                <KeepAlive name='welcome' cacheKey='welcome'>
-                                    <Welcome />
-                                </KeepAlive>
-                            }
-                        />
-                        {routers}
-                    </Routes>
-                </CSSTransition>
-            </SwitchTransition>
-        </AliveScope>
-    )
+  return (
+    // 路由缓存
+    <AliveScope>
+      {/* 路由切换过渡动画 */}
+      <SwitchTransition mode="out-in">
+        <CSSTransition
+          key={props.location.key}
+          timeout={200}
+          classNames="route"
+        >
+          <Routes location={props.location}>
+            {/* 所有路由都不命中时进入欢迎界面 */}
+            <Route
+              key='welcome'
+              path="*"
+              element={
+                <KeepAlive name='welcome' cacheKey='welcome'>
+                  <Welcome />
+                </KeepAlive>
+              }
+            />
+            {routers}
+          </Routes>
+        </CSSTransition>
+      </SwitchTransition>
+    </AliveScope>
+  )
 }
 
 export default WithRouter(RouterBody)
