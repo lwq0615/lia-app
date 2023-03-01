966c9062265583fd02d443947f7b62a693dba6d3
lwq7417 <1072864729@qq.com>
2023-03-01 11:59:29 +0800

    notice update

--- src/package/components/form/Upload.tsx
+++ src/package/components/form/Upload.tsx
@@ -26,7 +26,6 @@ export default class App extends React.Component<{
       name: 'file',
       beforeUpload: () => false,
       onChange: (info: any) => {
-        console.log(info);
         info.fileList.forEach((item: UploadFile) => {
           if(item.status === 'success'){
             return
