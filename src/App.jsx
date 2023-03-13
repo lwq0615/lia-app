d9e803bbcbd9815f205e3a551b734b8b406f1d2a
lwq7417 <1072864729@qq.com>
2023-03-13 10:22:00 +0800

    markdown代码高亮

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,25 @@ import zhCN from 'antd/lib/locale/zh_CN';
 import { Provider } from 'react-redux';
 import { baseRoutes, initRouter } from "./package/router/index";
 import store from './package/store';
+import { marked } from "marked"
+import hl from "highlight.js"
+
+const rendererMD = new marked.Renderer();
+
+marked.setOptions({
+  renderer: rendererMD,
+  highlight: function(code) {
+    return hl.highlightAuto(code).value;
+  },
+  pedantic: false,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false
+});
 
 moment.locale("zh-cn");
 
